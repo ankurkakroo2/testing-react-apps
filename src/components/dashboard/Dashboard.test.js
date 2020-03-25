2f5a0ec07a403300826d500171a116cf092f6dce
Rohit Kumar <rohkumar30@publicisgroupe.net>
2020-03-25 14:59:07 +0530

    added menu test cases

--- src/components/dashboard/Dashboard.test.js
+++ src/components/dashboard/Dashboard.test.js
@@ -1,18 +1,37 @@
 import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
 import Dashboard from "./Dashboard";
-import { render } from "@testing-library/react";
+import { setUserSession, removeUserSession, getUser } from "../../util/Common";
 
 let result;
+const user = "admin";
 beforeEach(() => {
-  result = render(<Dashboard />);
+  setUserSession("token", user);
+  const historyMock = { push: jest.fn() };
+  result = render(<Dashboard history={historyMock} />);
 });
 
-describe("Dashboard", () => {
-  test("render without crash", () => {
-    expect(result.getByText("Welcome", { exact: false })).toBeInTheDocument();
-  });
+/* afterEach(() => {
+  removeUserSession();
+}); */
 
+describe("Dashboard", () => {
   test("snapshot", () => {
     expect(result.asFragment()).toMatchSnapshot();
+  });
+
+  test("should render menu item with logged in user name", () => {
+    const { getByTestId } = result;
+    const signedInMenuItem = getByTestId("menu-item-username");
+    expect(signedInMenuItem.textContent).toBe(`Signed in as: ${user}`);
+    removeUserSession();
+  });
+
+  test("should fire logout event", () => {
+    const { getByText } = result;
+    const node = getByText("Logout");
+    fireEvent.click(node);
+    expect(getUser()).toBe(null);
   });
 });
